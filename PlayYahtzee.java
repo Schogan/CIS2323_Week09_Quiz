cda42534cb5152e9e849bb10d852987e4a7bc6af
Schogan <navy15@gmail.com>
2018-10-17 18:50:29 -0500

    Update broke it

--- PlayYahtzee.java
+++ PlayYahtzee.java
@@ -9,8 +9,34 @@ public class PlayYahtzee{
 		char choice = 'a';
 		Scanner inputDevice = new Scanner(System.in);
 		
+		
+		
 		while(cont){
 			System.out.println("=================================== \n");
+			System.out.println("Player 1 \n");
+			System.out.println("What would you like to do? \n");
+			System.out.println("r) Roll Dice \n");
+			System.out.println("c) Attempt to cheat \n");
+			System.out.println("q) Quit Game \n");
+			System.out.println("=================================== \n");
+			
+			choice = inputDevice.next().charAt(0);
+			inputDevice.nextLine();
+			
+			if (choice == 'r'){
+				System.out.println(game.oneToss());
+				
+			}
+			if (choice == 'c'){
+				System.out.println(game.oneCheat());
+				
+			}
+			if (choice == 'q'){
+				cont = false;
+			}
+			
+			System.out.println("=================================== \n");
+			System.out.println("Player 2 \n");
 			System.out.println("What would you like to do? \n");
 			System.out.println("r) Roll Dice \n");
 			System.out.println("c) Attempt to cheat \n");
--- Yahtzee/Yahtzee.java
+++ Yahtzee/Yahtzee.java
@@ -6,12 +6,48 @@ public class Yahtzee{
 	private Dice diceA;
 	private Dice diceB;
 	
+	public Player1[] rollOne = new Player1[10];
+	
+	public Player2[] rollTwo = new Player2[10];
+	
 	public Yahtzee(){
 		diceA = new Dice();
 		diceB = new Dice();
 		
 	}
 		
+	int countOne=0;
+		
+	public String oneToss(){
+		int dice1 = diceA.Roll();
+		int dice2 = diceB.Roll();
+		
+		
+		if (dice1 == 6 && dice2 == 6){
+			return " !!!YAHTZEE!!! ";
+		}else{
+			String one = String.valueOf(dice1);
+			String two = String.valueOf(dice2);
+			return one + " " + two;
+		}
+		rollOne[countOne] = new Yahtzee();
+		countOne++;
+	}
+	public String oneCheat(){
+		int dice1 = diceA.Roll();
+		try{		
+			if (dice1 == 6 || dice1 == 1){
+				return " !!!YAHTZEE!!! ";
+				rollOne[countOne] = 6,6;
+				countOne++;
+			}
+		}
+		catch(Exception youCheater){
+			System.out.println("You got caught!!!");
+		}
+		return "You just lost your hands you dirty cheater!!!";
+	}
+	
 	public String toss(){
 		int dice1 = diceA.Roll();
 		int dice2 = diceB.Roll();
